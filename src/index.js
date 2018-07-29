03d212d9e486db55c7dc5f55d52ee4442e47d7a1
vivianh2 <vivianh2@illinois.edu>
2018-07-29 20:22:49 +0800

    essay complete with draft for the bubble charts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './assets/lib/material-design-icons/css/material-design-iconic-font.css';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
